c8a4fba598436df8132c225c1a9300455b8d1351
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-08 15:34:07 +0000

    >  compile NEMU 3019244404 simpleedu Linux simpleedu 4.15.0-119-generic #120-Ubuntu SMP Fri Sep 18 11:08:20 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:34:07 up  1:23,  1 user,  load average: 1.06, 0.49, 0.24 853aca87222368b9bc6de0852d8bc25c13ae14a3

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -67,14 +67,17 @@ static int cmd_si(char *args){
 static int cmd_info(char *args)
 {
 	char *arg=strtok(NULL," ");
-	if(strcmp(arg,"r")==0)	
-	{
-                int i;
-		printf("Name\t Hex\t Decimal\n");
-		for(i=0;i<8;i++)
-			printf("%s 0x%x %d\n",regsl[i],cpu.gpr[i]._32,cpu.gpr[i]._32);
+	if(*arg == 'r'){
+		printf("eax 0x%x\n",cpu.eax);
+		printf("ecx 0x%x\n",cpu.ecx);
+		printf("edx 0x%x\n",cpu.edx);
+		printf("ebx 0x%x\n",cpu.ebx);
+		printf("esp 0x%x\n",cpu.esp);
+		printf("ebp 0x%x\n",cpu.ebp);
+		printf("esi 0x%x\n",cpu.esi);
+		printf("edi 0x%x\n",cpu.edi);
 	}
-	else if(strcmp(arg,"w")==0)
+	else if(*arg=='w')
 	{
 		print_wp();
 	}
